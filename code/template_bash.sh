f1f53cc1e7c9c73c55a97542360143e1c9eabc68
silvijas <suboticsilvija@yahoo.com>
2024-03-28 13:37:50 +0100

    Creating directories and template for bash script

---
+++ code/template_bash.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2024-2-7
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 01:00:00
+#SBATCH -J job_
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user suboticsilvija@yahoo.com
+#SBATCH --output=%x.%j.out
+
+
+# Load modules
+module load bioinfo-tools
+module load ....
+
+
+# Your commands
+<Command_1...>
+<Command_2...>
+
