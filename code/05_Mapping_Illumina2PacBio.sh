e6e3acb9139ae10c2582fa06e09c540e23e84dbb
silvijas <suboticsilvija@yahoo.com>
2024-04-09 15:15:53 +0200

    05 Script updated with code for converting SAM -> BAM file. Output file from job in the results 05 folder.

--- code/05_Mapping_Illumina2PacBio.sh
+++ code/05_Mapping_Illumina2PacBio.sh
@@ -4,7 +4,7 @@
 #SBATCH -p core
 #SBATCH -n 8
 #SBATCH -t 02:00:00
-#SBATCH -J job_mapping
+#SBATCH -J job_03_convert_sort_index
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user suboticsilvija@yahoo.com
 #SBATCH --output=%x.%j.out
@@ -12,7 +12,7 @@
 
 # Load modules
 module load bioinfo-tools
-module load bwa/0.7.17 bwa-mem2
+module load bwa/0.7.17 bwa-mem2 samtools/1.19
 
 
 # Stop executing, if there is error
@@ -31,5 +31,11 @@ ILL2=~/GenomeAnalysis/data/DNA/Illumina/E745-1.L500_SZAXPI015146-56_2_clean.fq.g
 
 #bwa index $ASMB 
 
-bwa mem -t 8 $ASMB $ILL1 $ILL2 > "${ODir}/aln_Ill2PacBio.sam"
+#bwa mem -t 8 $ASMB $ILL1 $ILL2 > "${ODir}/aln_Ill2PacBio.sam"
 
+## Converting file SAM -> BAM	From :: https://www.htslib.org/doc/samtools-view.html
+samtools view -bo "${ODir}/aln_Ill2PacBio.bam" "${ODir}/aln_Ill2PacBio.sam"
+
+## Sorting and indexing sorted BAM file, as it is generally the preferred format for further analysis.
+samtools sort "${ODir}/aln_Ill2PacBio.bam" -o "${ODir}/aln_Ill2PacBio_sorted.bam"
+samtools index "${ODir}/aln_Ill2PacBio_sorted.bam"
